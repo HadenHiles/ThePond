da4a9ba0d9c7c9173edf61ab6c5fb37f0f090255
Haden Hiles <contact@hadenhiles.com>
2019-12-06 12:24:24 -0500

    Add THEPOND_DB_PORT environment variable to DB_HOST global var instead of DB_NAME

--- wp-config/wp-db-production.php
+++ wp-config/wp-db-production.php
@@ -3,7 +3,7 @@
 if (!defined('ABSPATH')) exit();
 
 /** The name of the database for WordPress */
-define( 'DB_NAME', getenv('THEPOND_DB_NAME') . ':' . getenv('THEPOND_DB_PORT') );
+define( 'DB_NAME', getenv('THEPOND_DB_NAME') );
 
 /** MySQL database username */
 define( 'DB_USER', getenv('THEPOND_DB_USER') );
@@ -12,7 +12,7 @@ define( 'DB_USER', getenv('THEPOND_DB_USER') );
 define( 'DB_PASSWORD', getenv('THEPOND_DB_PASSWORD') );
 
 /** MySQL hostname */
-define( 'DB_HOST', getenv('THEPOND_DB_HOST') );
+define( 'DB_HOST', getenv('THEPOND_DB_HOST') . ':' . getenv('THEPOND_DB_PORT') );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', getenv('THEPOND_DB_CHARSET') );
