b03c87be4b051626354fc6f43a653d8a78c48df1
Haden Hiles <haden@hadenhiles.com>
2019-12-19 17:52:40 -0500

    Create shortcode for pulling learndash courses and grouping by course category (#3)
    Html is design is implemented using melting_pot membership.css

---
+++ wp-content/themes/meltingpot-child/shortcodes.php
@@ -0,0 +1,109 @@
+<?php
+add_shortcode('ld_course_tags', 'learndash_course_tags');
+function learndash_course_tags($atts = [], $content = null, $tag = ''){
+  $tmp_atts = array_change_key_case((array)$atts, CASE_LOWER);
+
+  // override default attributes with user attributes
+  $atts = shortcode_atts([
+    'categories' => 'skating,stickhandling,shooting,passing',
+  ], $tmp_atts, $tag);
+
+  $categories = explode(',', $atts['categories']);
+
+  $courses_by_category = array();
+  foreach ($categories as $category) {
+    $courses_by_category += [$category => array()];
+  }
+
+  $output = '';
+
+  $args = array(
+    'post_type' => array('sfwd-courses'),
+		'meta_query' => array(
+      'relation' => 'AND',
+			array(
+				'key' => 'available',
+				'value' => true,
+				'compare' => '=',
+			),
+			array(
+				'key' =>'hide_from_feed',
+				'value' => true,
+				'compare' =>'!=',
+		  )
+		),
+    'tax_query' => array(
+      'relation' => 'OR',
+      array(
+        'taxonomy' => 'ld_course_category',
+        'field' => 'slug',
+        'terms' => $categories,
+        'operator' => 'IN',
+      )
+    ),
+  );
+  $course_feed = new WP_Query( $args );
+
+	if ($course_feed->have_posts()) {
+		while ($course_feed->have_posts()) {
+			$course_feed->the_post();
+      $course = get_post(get_the_ID());
+			$imgID  = get_post_thumbnail_id($post->ID);
+			$img    = wp_get_attachment_image_src($imgID,'full', false, '');
+			$imgAlt = get_post_meta($imgID,'_wp_attachment_image_alt', true);
+			if (empty($img[0])) {
+				$img[0] = DEFAULT_IMG;
+			}
+
+      $course->img = $img;
+      $course->imgAlt = $imgAlt;
+			$course->course_id = get_the_ID();
+			$course->user_id =  get_current_user_id();
+      $course->post_url = get_permalink($course);
+
+			$course->course_status = learndash_course_status( $course->course_id, $course->user_id );
+			$course->course_steps_count = learndash_get_course_steps_count( $course->course_id );
+			$course->completed = learndash_course_get_completed_steps( $course->user_id, $course->course_id );
+
+			$ld_course_steps_object = LDLMS_Factory_Post::course_steps( $course->course_id );
+			$total = $ld_course_steps_object->get_steps_count();
+
+			if ($total > 0) {
+		     $percentage = intval( $course->completed * 100 / $total );
+			   $percentage = ( $percentage > 100 ) ? 100 : $percentage;
+			} else {
+			     $percentage = 0;
+			}
+      $course->percentage = $percentage;
+
+      $course_category_list = get_the_terms($post, 'ld_course_category');
+      $course_category_slugs = array();
+      foreach ($course_category_list as $course_category_item) {
+        array_push($course_category_slugs, $course_category_item->slug);
+      }
+
+      foreach ($courses_by_category as $key => $value) {
+        if (in_array($key, $course_category_slugs)) {
+          array_push($courses_by_category[$key], $course);
+        }
+      }
+		}
+	}
+
+  $output .= '<div class="course-container">';
+  foreach ($courses_by_category as $key => $value) {
+    foreach ($courses_by_category[$key] as $c) {
+      $output .=  '<div class="inner">'.
+                    '<a class="course-item" href="' . $c->post_url . '" style="background-image: url(\''. $c->img[0] . '\')">'.
+                      '<h5>' . $c->post_title . '</h5>'.
+                      '<div class="overlay"></div>'.
+                      '<div class="progress-bar" style="width: ' . $c->percentage . '%">' . $c->percentage . '%</div>'.
+                    '</a>'.
+                  '</div>';
+    }
+  }
+  $output .= '</div>';
+
+  echo $output;
+}
+?>
