7fc0e8a7c68885365da5539656a9a80ebce6b25d
hoda <hadenhiles@gmail.com>
2020-02-26 16:01:50 -0500

    Manually trigger supervisor hook on droplet any time that elementor plugin data is updated
    Particularly for any custom css styles

--- wp-content/themes/meltingpot-child/functions.php
+++ wp-content/themes/meltingpot-child/functions.php
@@ -9,6 +9,31 @@ function mysql_set_sql_mode_traditional() {
     $wpdb->query("SET SESSION sql_mode = ''");
 }
 
+/**
+* Manually trigger supervisord hook running on thepond DO droplet any time that elementor data is updated
+* - this is required so that styles will be applied
+* TODO: figure out a way to automatically expire the cache of modified files
+*/
+add_action( 'elementor/editor/after_save', function() {
+  // Curl code pulled from postman test
+  $curl = curl_init();
+
+  curl_setopt_array($curl, array(
+    CURLOPT_URL => "http://178.128.232.27:9000/hooks/updatecdn",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "POST",
+  ));
+
+  $response = curl_exec($curl);
+
+  curl_close($curl);
+});
+
 add_action( 'wp_enqueue_scripts', 'child_theme_enqueue_styles' );
 function child_theme_enqueue_styles() {
 
