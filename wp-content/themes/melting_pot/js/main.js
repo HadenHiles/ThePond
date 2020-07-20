b94aadbfc6f0037c3a7b986fcaf8d72f265bc969
HadenHiles <haden@howtohockey.com>
2020-07-20 15:32:04 -0400

    Tweak functionality/layout of skills search and filtering to better suite mobile

--- wp-content/themes/melting_pot/js/main.js
+++ wp-content/themes/melting_pot/js/main.js
@@ -1,59 +1,113 @@
-jQuery(document).ready(function () {
-	if (jQuery('.course-container').length > 0) {
-		const slider = document.querySelector('.course-container');
-		let isDown = false;
-		let startX;
-		let scrollLeft;
+(function($) {
+	$(document).ready(function() {
+		if ($('.course-container').length > 0) {
+			const slider = document.querySelector('.course-container');
+			let isDown = false;
+			let startX;
+			let scrollLeft;
+	
+			slider.addEventListener('mousedown', (e) => {
+			  isDown = true;
+			  slider.classList.add('active');
+			  startX = e.pageX - slider.offsetLeft;
+			  scrollLeft = slider.scrollLeft;
+			});
+			slider.addEventListener('mouseleave', () => {
+			  isDown = false;
+			  slider.classList.remove('active');
+			});
+			slider.addEventListener('mouseup', () => {
+			  isDown = false;
+			  slider.classList.remove('active');
+			});
+			slider.addEventListener('mousemove', (e) => {
+			  if(!isDown) return;
+			  e.preventDefault();
+			  const x = e.pageX - slider.offsetLeft;
+			  const walk = (x - startX) * 2; //scroll-fast
+			  slider.scrollLeft = scrollLeft - walk;
+			});
+	
+			// Set the minimum hight for the course container
+			$('.course-container').css('min-height', $('.section-icons').css('height'));
+	
+			var scrollDistance = $('.course-item').width() * 1.5;
+			$('#scroll-left-btn').click(function () {
+				$('.course-container').animate({
+				scrollLeft: slider.scrollLeft - scrollDistance
+			}, 300);
+			});
+			$('#scroll-right-btn').click(function () {
+				$('.course-container').animate({
+				scrollLeft: slider.scrollLeft + scrollDistance
+			}, 300);
+			});
+		}
+	
+		//TODO: This is a hack - Move user rank to within profile info in community profile sidebar
+		$('.yzb-user-status').insertAfter($('.yzb-head-username'));
+		if ($('#user-rank').length > 0) {
+			$('#user-rank').prependTo($('.yzb-user-status'));
+			$('#user-rank').css({'opacity': '1'});
+		}
+		$('.yzb-user-status').css({'opacity': '1'});
+	
+		$('.topmemberNav .yz-primary-nav-settings' ).unbind('click');
+		$('.topmemberNav .yz-primary-nav-area').click(function(e) {
+			e.preventDefault();
+			window.location.href = $(this).closest('a').attr('href');
+		});
 
-		slider.addEventListener('mousedown', (e) => {
-		  isDown = true;
-		  slider.classList.add('active');
-		  startX = e.pageX - slider.offsetLeft;
-		  scrollLeft = slider.scrollLeft;
-		});
-		slider.addEventListener('mouseleave', () => {
-		  isDown = false;
-		  slider.classList.remove('active');
-		});
-		slider.addEventListener('mouseup', () => {
-		  isDown = false;
-		  slider.classList.remove('active');
-		});
-		slider.addEventListener('mousemove', (e) => {
-		  if(!isDown) return;
-		  e.preventDefault();
-		  const x = e.pageX - slider.offsetLeft;
-		  const walk = (x - startX) * 2; //scroll-fast
-		  slider.scrollLeft = scrollLeft - walk;
+		// Skills Vault
+		var skillsVaultTable = $('#skills-vault-table').DataTable({
+			paging: false,
+			searching: true,
+			// responsive: {
+			// 	details: {
+			// 		display: $.fn.dataTable.Responsive.display.childRowImmediate,
+			// 		type: 'column',
+			// 		target: 'tr'
+			// 	},
+			// 	columns: [
+			// 		{ responsivePriority: 1 },
+			// 		{ responsivePriority: 2 },
+			// 		{ responsivePriority: 3 },
+			// 		{ responsivePriority: 4 }
+			// 	]
+			// },
+			scrollX: true,
+			autoFill: false,
+			info: false,
+			fixedHeader: false,
+			language: {
+				searchPlaceholder: "Find a Skill",
+				emptyTable: "There are no skills in the skills vault yet.",
+				zeroRecords: "No skills were found."
+			},
+			oLanguage: {
+				sSearch: ""
+			},
+			order: [1, 'desc']
 		});
 
-		// Set the minimum hight for the course container
-		jQuery('.course-container').css('min-height', jQuery('.section-icons').css('height'));
-
-		var scrollDistance = jQuery('.course-item').width() * 1.5;
-		jQuery('#scroll-left-btn').click(function () {
-			jQuery('.course-container').animate({
-	        scrollLeft: slider.scrollLeft - scrollDistance
-	    }, 300);
+		$('#skills-vault-table_filter input[type="search"]').on('keyup', function() {
+			$('#skill-types-filter a.nav-link.active').removeClass('active');
+			skillsVaultTable.columns().search('').draw();
+			skillsVaultTable.search($(this).val());
 		});
-		jQuery('#scroll-right-btn').click(function () {
-			jQuery('.course-container').animate({
-	        scrollLeft: slider.scrollLeft + scrollDistance
-	    }, 300);
+
+		$('#skill-types-filter').on('click', 'li a.nav-link', function() {
+			if (!$(this).hasClass('active')) {
+				$('#skill-types-filter a.nav-link.active').removeClass('active');
+				skillsVaultTable.column(3).search($(this).text()).draw();	
+				$(this).addClass('active');
+			} else {
+				$(this).removeClass('active');
+				skillsVaultTable.columns().search('').draw();
+			}
+
+			var searchTerm = skillsVaultTable.columns(3).search()[0];
+			$('.dataTables_empty').text('No skills were found for "' + searchTerm + '"');
 		});
-	}
-
-	//TODO: This is a hack - Move user rank to within profile info in community profile sidebar
-	jQuery('.yzb-user-status').insertAfter(jQuery('.yzb-head-username'));
-	if (jQuery('#user-rank').length > 0) {
-		jQuery('#user-rank').prependTo(jQuery('.yzb-user-status'));
-		jQuery('#user-rank').css({'opacity': '1'});
-	}
-	jQuery('.yzb-user-status').css({'opacity': '1'});
-
-	jQuery('.topmemberNav .yz-primary-nav-settings' ).unbind('click');
-	jQuery('.topmemberNav .yz-primary-nav-area').click(function(e) {
-		e.preventDefault();
-		window.location.href = jQuery(this).closest('a').attr('href');
 	});
-});
+})(jQuery);
