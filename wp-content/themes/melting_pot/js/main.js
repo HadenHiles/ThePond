6523f4614b72638601ebde9c52bb8f55c48a4aa5
HadenHiles <haden@howtohockey.com>
2020-07-21 17:29:40 -0400

    Add code to support full screen images just by clicking on them

--- wp-content/themes/melting_pot/js/main.js
+++ wp-content/themes/melting_pot/js/main.js
@@ -44,18 +44,32 @@
 			});
 		}
 	
-		//TODO: This is a hack - Move user rank to within profile info in community profile sidebar
-		$('.yzb-user-status').insertAfter($('.yzb-head-username'));
-		if ($('#user-rank').length > 0) {
-			$('#user-rank').prependTo($('.yzb-user-status'));
-			$('#user-rank').css({'opacity': '1'});
-		}
-		$('.yzb-user-status').css({'opacity': '1'});
-	
-		$('.topmemberNav .yz-primary-nav-settings' ).unbind('click');
-		$('.topmemberNav .yz-primary-nav-area').click(function(e) {
-			e.preventDefault();
-			window.location.href = $(this).closest('a').attr('href');
+		// Option to allow full screen image on click
+		$('img[data-enlargable]').addClass('img-enlargable').click(function(){
+			var src = $(this).attr('src');
+			var modal;
+			
+			function removeModal(){ 
+				modal.remove(); 
+				$('body').off('keyup.modal-close'); 
+			}
+			
+			modal = $('<div>').css({
+				background: 'RGBA(0,0,0,.75) url('+src+') no-repeat center',
+				backgroundSize: 'contain',
+				width:'100%', height:'100%',
+				position:'fixed',
+				zIndex:'10000',
+				top:'0', left:'0',
+				cursor: 'zoom-out'
+			}).click(function(){
+				removeModal();
+			}).appendTo('body');
+			
+			//handling ESC
+			$('body').on('keyup.modal-close', function(e){
+			  if(e.key==='Escape'){ removeModal(); } 
+			});
 		});
 
 		// Skills Vault
