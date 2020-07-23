259af5a1c9b68011a3369f789e99471d18452d44
HadenHiles <haden@howtohockey.com>
2020-07-23 17:28:46 -0400

    Display prerequisite skills on course page

--- wp-content/themes/melting_pot/learndash/layout/single-layout-one.php
+++ wp-content/themes/melting_pot/learndash/layout/single-layout-one.php
@@ -21,20 +21,47 @@ if(have_posts()): while (have_posts()): the_post();
 
 ?>
 <div class="large-8 medium-7 columns">
-<main role="main">
-<article>
-<div class="CourseContent">
-<?php get_template_part('template-parts/courses/lesson-topic-fields'); ?>
-<?php get_template_part('template-parts/courses/course-downloads'); ?>
-	
-<?php get_template_part('template-parts/courses/course-forum-link'); ?>
-	
-	
-<?php get_template_part('template-parts/courses/coursehistory'); ?> 
-	</div>
-	</article>
-</main>
-
+	<main role="main">
+		<article>
+			<div class="CourseContent">
+				<h2 style="margin-bottom: 5px;">Prerequisite Skills</h2>
+				<div class="bootstrap-styles skills-list">
+					<?php
+					$prerequisiteSkills = get_field('prerequisite_skills', $post->ID);
+					foreach($prerequisiteSkills as $prerequisiteSkill) {
+						$performanceLevels = get_the_terms( $prerequisiteSkill->ID, 'performance-level' ); 
+						$performanceLevelString = '';
+						if(sizeof($performanceLevels) > 0) {
+							$count = 0;
+							foreach($performanceLevels as $performanceLevel) {
+								if (++$count > 1 && $count <= sizeof($performanceLevels)) {
+									$performanceLevelString .= ', ';
+								}
+								$performanceLevelString .= $performanceLevel->name;
+							}
+						}
+						?>
+						<div class="card skill">
+							<div class="card-body content">
+								<a href="<?=get_post_permalink($prerequisiteSkill->ID)?>" class="ghost"></a>
+								<a href="<?=get_post_permalink($prerequisiteSkill->ID)?>" class="title"><?=get_the_title($prerequisiteSkill->ID)?></a>
+								<span class="level"><?=$performanceLevelString?></span>
+							</div>
+						</div>
+						<?php
+					}
+					?>
+				</div>
+				<?php get_template_part('template-parts/courses/lesson-topic-fields'); ?>
+				<?php get_template_part('template-parts/courses/course-downloads'); ?>
+					
+				<?php get_template_part('template-parts/courses/course-forum-link'); ?>
+					
+					
+				<?php get_template_part('template-parts/courses/coursehistory'); ?> 
+			</div>
+		</article>
+	</main>
 </div>
 	
 <div class="large-4 medium-5 columns">
