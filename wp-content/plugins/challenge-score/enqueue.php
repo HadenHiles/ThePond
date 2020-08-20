b90ea872268e3101ea3d240f4324cb9db19b60aa
HadenHiles <haden@howtohockey.com>
2020-08-20 13:41:43 -0400

    Make sure jQuery is enqueued

--- wp-content/plugins/challenge-score/enqueue.php
+++ wp-content/plugins/challenge-score/enqueue.php
@@ -2,6 +2,7 @@
 function load_challenge_score_scripts() {
     $plugin_url = plugin_dir_url( __FILE__ );
 
+    wp_enqueue_script( 'jquery', get_template_directory_uri() . '/js/vendor/jquery.min.js', array(), null, true);
     wp_enqueue_style( 'challenge_score_styles', $plugin_url . 'css/style.css' );
     wp_enqueue_script( 'challenge_score_scripts', $plugin_url . 'js/score-management.js' );
 }
