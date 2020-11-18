aa0317062416b0055f4a9a8d4c5703b7d3509b08
HadenHiles <haden@howtohockey.com>
2020-11-18 12:44:48 -0500

    Add custom memberpress email template

---
+++ wp-content/themes/meltingpot-child/memberpress/emails/template.php
@@ -0,0 +1,32 @@
+<?php if(!defined('ABSPATH')) {die('You are not allowed to call this page directly.');} ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" "http://www.w3.org/TR/REC-html40/loose.dtd">
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html;UTF-8">
+  </head>
+  <body style="background: #eaeaea; font: 13px 'Lucida Grande', 'Lucida Sans Unicode', Tahoma, Verdana, sans-serif; padding: 5px 0 10px" bgcolor="#eaeaea">
+    <style type="text/css">
+    body {
+      font: 13px "Noto Sans", "Lucida Grande", "Lucida Sans Unicode", Tahoma, Verdana, sans-serif; background-color: #efefef; padding: 5px 0 10px 0;
+    }
+    h1 {
+        font: 40px "Teko", "Lucida Grande", "Lucida Sans Unicode", Tahoma, Verdana, sans-serif;
+    }
+    </style>
+    <header style="width: 100%; background: #14345a; padding: 20px; text-align: center;">
+    <div class="logo" style="width: 150px; margin: 0 auto;">
+        <img src="https://cdn.thepond.howtohockey.com/2020/11/THEPOND_RGB_WHITE_WORDMARK_RAW.png" alt="The Pond" style="max-width: 100%;" />
+    </div>
+    </header>
+
+    <div style="position: relative; z-index: 1;">
+      <?php echo $body; ?>
+    </div>
+
+    <footer style="width: 100%; padding: 20px 0; background: #eaeaea; float: left;">
+      <div style="float: right; width: 50%; text-align: right; position: absolute; right: 0; bottom: 0; z-index: 0;">
+        <img src="https://thepond.howtohockey.com/wp-content/themes/meltingpot-child/images/snowbank.png" alt="The Pond" style="width: 500px; max-width: 100%;" />
+      </div>
+    </footer>
+  </body>
+</html>
