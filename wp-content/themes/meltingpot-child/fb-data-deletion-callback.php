9f674bd2951607f0362c1625ad1cee7646fbb187
HadenHiles <haden@howtohockey.com>
2020-12-07 16:51:18 -0500

    Bogus facebook data deletion callback page

---
+++ wp-content/themes/meltingpot-child/fb-data-deletion-callback.php
@@ -0,0 +1,41 @@
+<?php
+header('Content-Type: application/json');
+
+$signed_request = $_POST['signed_request'];
+$data = parse_signed_request($signed_request);
+$user_id = $data['user_id'];
+
+// Start data deletion
+
+$status_url = 'https://thepond.howtohockey.com/?id=123'; // URL to track the deletion
+$confirmation_code = 'abc123'; // unique code for the deletion request
+
+$data = array(
+  'url' => $status_url,
+  'confirmation_code' => $confirmation_code
+);
+echo json_encode($data);
+
+function parse_signed_request($signed_request) {
+  list($encoded_sig, $payload) = explode('.', $signed_request, 2);
+
+  $secret = "c0b8e304639f0954b4636c5105822bac"; // Use your app secret here
+
+  // decode the data
+  $sig = base64_url_decode($encoded_sig);
+  $data = json_decode(base64_url_decode($payload), true);
+
+  // confirm the signature
+  $expected_sig = hash_hmac('sha256', $payload, $secret, $raw = true);
+  if ($sig !== $expected_sig) {
+    error_log('Bad Signed JSON signature!');
+    return null;
+  }
+
+  return $data;
+}
+
+function base64_url_decode($input) {
+  return base64_decode(strtr($input, '-_', '+/'));
+}
+?>
