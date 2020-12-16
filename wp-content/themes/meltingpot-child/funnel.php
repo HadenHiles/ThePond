20cb93f06c642b722ee8d4514d4de469a4cbed02
HadenHiles <haden@howtohockey.com>
2020-12-16 15:34:25 -0500

    Ensure that redirect_to functionality works for users who have a recent_login cookie

--- wp-content/themes/meltingpot-child/funnel.php
+++ wp-content/themes/meltingpot-child/funnel.php
@@ -1,7 +1,7 @@
 <?php
 require_once('../../../wp-load.php');
 
-$redirectUrl = $_COOKIE['redirect_to'];
+$redirectUrl = !empty($_GET['redirect_to']) ? $_GET['redirect_to'] : $_COOKIE['redirect_to'];
 setcookie('redirect_to', null, -1, '/');
 
 $chosenMembership = $_COOKIE['selected_membership'];
@@ -9,7 +9,6 @@ setcookie('selected_membership', null, -1, '/');
 $hasActiveSubscription = false;
 
 $recentLogin = $_COOKIE['recent_login'];
-setcookie('recent_login', null, -1, '/');
 
 $meprUser = new MeprUser(get_current_user_id());
 
@@ -18,9 +17,10 @@ $activeSubscriptions = $meprUser->active_product_subscriptions('ids');
 
 // Check if the unsigned in user has recently logged in or not
 // * Is the user of this device a member or not -> redirect to login instead of signup
-if (empty(get_current_user_id()) && !empty($recentLogin) && $recentLogin == true) {
+if (empty(get_current_user_id()) && $recentLogin == 1) {
+    setcookie('redirect_to', $redirectUrl, -1, '/');
     header('location: /login');
-} else if (empty(get_current_user_id()) && empty($recentLogin)) {
+} else if (empty(get_current_user_id()) && $recentLogin != 1) {
     header('location: /#choose-your-subscription');
 } else {
     // Set the recent_login cookie so the user will be redirected to login instead of sign up if they are a member
