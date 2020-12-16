ddc7f654449d272e11dc6b71c0184929eab9a397
HadenHiles <haden@howtohockey.com>
2020-12-16 15:20:53 -0500

    Direct users who have not ever been a member to the choose-your-subscription section on the landing page
    Users that are members will have a cookie stored on their machine for 30 days. As long as they sign in once within 30 days it will continue to redirect them to login when viewing unauthorized content. Ex. They bookmarked pages but their session expired and they need to login again.

--- wp-content/themes/meltingpot-child/funnel.php
+++ wp-content/themes/meltingpot-child/funnel.php
@@ -8,28 +8,40 @@ $chosenMembership = $_COOKIE['selected_membership'];
 setcookie('selected_membership', null, -1, '/');
 $hasActiveSubscription = false;
 
+$recentLogin = $_COOKIE['recent_login'];
+setcookie('recent_login', null, -1, '/');
+
 $meprUser = new MeprUser(get_current_user_id());
 
 $subscriptions = $meprUser->active_product_subscriptions('ids', false, false);
 $activeSubscriptions = $meprUser->active_product_subscriptions('ids');
 
-if (empty($subscriptions)) {
-    // Check if the user already chose a subscription
-    if (!empty($chosenMembership)) {
-        header('location: ' . $chosenMembership);
-    } else {
-        header('location: /choose-your-subscription/');
-    }
-} else if (!empty($activeSubscriptions)) {
-    if(empty($redirectUrl)) {
-        header('location: /member-dashboard');
-    } else {
-        header("location: $redirectUrl");
-    }
+// Check if the unsigned in user has recently logged in or not
+// * Is the user of this device a member or not -> redirect to login instead of signup
+if (empty(get_current_user_id()) && !empty($recentLogin) && $recentLogin == true) {
+    header('location: /login');
+} else if (empty(get_current_user_id()) && empty($recentLogin)) {
+    header('location: /#choose-your-subscription');
 } else {
-    header('location: /account?action=subscriptions');
-}
+    // Set the recent_login cookie so the user will be redirected to login instead of sign up if they are a member
+    setcookie("recent_login", true, strtotime( '+30 days' ), "/"); // expire in 30 days
 
-get_footer("members");
+    if (empty($subscriptions)) {
+        // Check if the user already chose a subscription
+        if (!empty($chosenMembership)) {
+            header('location: ' . $chosenMembership);
+        } else {
+            header('location: /#choose-your-subscription');
+        }
+    } else if (!empty($activeSubscriptions)) {
+        if(empty($redirectUrl)) {
+            header('location: /member-dashboard');
+        } else {
+            header("location: $redirectUrl");
+        }
+    } else {
+        header('location: /account?action=subscriptions');
+    }
+}
 
 ?>
