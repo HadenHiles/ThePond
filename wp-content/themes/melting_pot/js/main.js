92fdb090df28d2ee0fc02f4f71e3988daf6ff647
HadenHiles <haden@howtohockey.com>
2020-12-30 10:37:25 -0500

    Show notification bar if different than last closed notification (allows us to publish time sensitive announcements without having to wait for their cookie to expire)

--- wp-content/themes/melting_pot/js/main.js
+++ wp-content/themes/melting_pot/js/main.js
@@ -334,10 +334,11 @@
 	$(document).ready(() => {
 		if ($('#member-dashboard').length > 0) {
 			var notificationClosed = getCookie("notification_closed");
-			if (notificationClosed) {
+			if (notificationClosed == $('.easy-notification-bar-message').text().trim()) {
 				$('.easy-notification-bar').attr('style', 'display: none !important');
 			} else {
 				$('.easy-notification-bar').attr('style', 'display: block !important');
+				$('.easy-notification-bar-message').append('<a href="#" id="close-easy-notification"></a>');
 				var $closeBtn = $('#close-easy-notification');
 				$('.easy-notification-bar').css({ "position": "relative" });
 				$closeBtn.html('<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="times" class="svg-inline--fa fa-times fa-w-11" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512"><path fill="currentColor" d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"></path></svg>');
@@ -349,7 +350,7 @@
 					if ($('.easy-notification-bar').css('position') == 'fixed' && $('.easy-notification-bar').is(":hidden")) {
 						$('section.memberbenefits').attr('style', '');
 					}
-					setCookie("notification_closed", true, 1);
+					setCookie("notification_closed", $('.easy-notification-bar-message').text().trim(), 1);
 				});
 			}
 
