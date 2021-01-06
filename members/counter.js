57dd55094b57c836730adf67a4ae5b2b73fcdd1d
HadenHiles <haden@howtohockey.com>
2021-01-06 14:19:25 -0500

    Use cookie to store member_count so that if you refresh the page it will still play the goal horn for new members

--- members/counter.js
+++ members/counter.js
@@ -1,5 +1,5 @@
 (($) => {
-    var count = 0;
+    var count = getCookie("member_count") ?? 0;
     var firstLoad = true;
 
     function flipTo(digit, n) {
@@ -42,7 +42,8 @@
                 if (!(response instanceof Object)) {
                     response = JSON.parse(response);
                 }
-                count = response.count != null ? response.count : 0;
+                
+                setCookie("member_count", response.count != null ? response.count : 0, 2);
                 fullCount = count.toString().padStart(6, "0");
 
                 updateDigit('six', fullCount.charAt(0), flip);
@@ -77,3 +78,26 @@
         }, 60000);
     });
 })(jQuery);
+
+function setCookie(name, value, minutes) {
+	var expires = "";
+	if (days) {
+		var date = new Date();
+		date.setTime(date.getTime() + (minutes * 60 * 1000));
+		expires = "; expires=" + date.toUTCString();
+	}
+	document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+function getCookie(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for (var i = 0; i < ca.length; i++) {
+		var c = ca[i];
+		while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+	}
+	return null;
+}
+function eraseCookie(name) {
+	document.cookie = name + '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
