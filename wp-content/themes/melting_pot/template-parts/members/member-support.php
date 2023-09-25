62e443e79371996d3d953ce7870ecaa57a3568ea
HadenHiles <haden@howtohockey.com>
2023-09-25 16:25:25 -0400

    Replace gravity forms shortcode with wpforms shortcode - gravity forms had security vulnerabilities without paying for license and latest version.

--- wp-content/themes/melting_pot/template-parts/members/member-support.php
+++ wp-content/themes/melting_pot/template-parts/members/member-support.php
@@ -1,24 +1,24 @@
 <?php ?>
 <main role="main">
-<article>
-<?php if( get_field('sub_header') ): ?>
-<h2><?php the_field('sub_header'); ?></h2>
-<?php endif; ?>
-	
-<?php if( get_field('above_media') ): ?>
-<?php the_field('above_media'); ?>
-<?php endif; ?>
-<div class="faqs">
-<?php get_template_part('template-parts/content-blocks/accordian'); ?>
-</div>
-<?php if( get_field('below_media') ): ?>
-<?php the_field('below_media'); ?>
-<?php endif; ?>
-<?php 
-$FormID = get_field('form_id'); 
-?>
-<div class="membSupportForm">
-<?php echo do_shortcode('[gravityform id="' . $FormID .  '" title="false" description="false" tabindex="10"]'); ?>
-</div>
-</article>
+    <article>
+        <?php if (get_field('sub_header')) : ?>
+            <h2><?php the_field('sub_header'); ?></h2>
+        <?php endif; ?>
+
+        <?php if (get_field('above_media')) : ?>
+            <?php the_field('above_media'); ?>
+        <?php endif; ?>
+        <div class="faqs">
+            <?php get_template_part('template-parts/content-blocks/accordian'); ?>
+        </div>
+        <?php if (get_field('below_media')) : ?>
+            <?php the_field('below_media'); ?>
+        <?php endif; ?>
+        <?php
+        $FormID = get_field('form_id');
+        ?>
+        <div class="membSupportForm">
+            <?php echo do_shortcode('[wpforms id="' . $FormID .  '" title="false"]'); ?>
+        </div>
+    </article>
 </main>
